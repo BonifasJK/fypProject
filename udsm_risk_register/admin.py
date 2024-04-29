e07e6970fac115a171e43b967581c78a4ecf3543
BonifasJK <bonifasjkomba@gmail.com>
2024-04-29 20:00:05 +0300

    sett pagination on units table/department

--- udsm_risk_register/admin.py
+++ udsm_risk_register/admin.py
@@ -4,6 +4,9 @@ from . import models
 # Register your models here.
 
 class UnitsAdmin(admin.ModelAdmin):
+    # To Sett Pagination
+    list_per_page = 6
+    list_max_show_all = 6
     list_display = ('Units',)
 admin.site.register(models.Units, UnitsAdmin)
 class UsersAdmin(admin.ModelAdmin):
