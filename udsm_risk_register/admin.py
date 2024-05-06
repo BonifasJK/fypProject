3d665829dda66ab394f428a4e6660b9f7a096a6a
BonifasJK <bonifasjkomba@gmail.com>
2024-05-06 22:44:33 +0300

    loginPage updated

--- udsm_risk_register/admin.py
+++ udsm_risk_register/admin.py
@@ -3,6 +3,13 @@ from . import models
 
 # Register your models here.
 
+class RiskAdmin(admin.ModelAdmin):
+    # To Sett Pagination
+    list_per_page = 6
+    list_max_show_all = 6
+    list_display = ('username','job_title','department','total_risks','status','last_updated')
+admin.site.register(models.Risk, RiskAdmin)
+
 class UnitsAdmin(admin.ModelAdmin):
     # To Sett Pagination
     list_per_page = 6
