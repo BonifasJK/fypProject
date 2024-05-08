be85553a33ea2e7eaf9c40c1f53ce5798c8cb977
samrickie~ <esamwel893@gmail.com~>
2024-05-08 22:42:10 +0300

    models.py updated

--- udsm_risk_register/admin.py
+++ udsm_risk_register/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . import models
+from .models import Mitigation, RiskDetails
 
 # Register your models here.
 
@@ -7,7 +8,7 @@ class RiskAdmin(admin.ModelAdmin):
     # To Sett Pagination
     list_per_page = 6
     list_max_show_all = 6
-    list_display = ('reporter','role','unit','status','last_updated')
+    list_display = ('title','reporter','Description', 'Details', 'status', 'last_updated', 'likelihood', 'impact', 'mitigation')
 
 admin.site.register(models.Risk, RiskAdmin)
 
@@ -17,9 +18,14 @@ class UnitsAdmin(admin.ModelAdmin):
     list_max_show_all = 6
     list_display = ('Units',)
 admin.site.register(models.Unit, UnitsAdmin)
+
 class UsersAdmin(admin.ModelAdmin):
     list_display = ('fullname','role','Unit','total_risks')
 admin.site.register(models.User, UsersAdmin)
+
+admin.site.register(Mitigation)
+admin.site.register(RiskDetails)
+
     
 
     
--- udsm_risk_register/models.py
+++ udsm_risk_register/models.py
@@ -1,67 +1,76 @@
 from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 from django.db.models import Count
+from django.conf import settings
 
-# Create your models here.
+
 UnitList = (
-    ('Human Resource','Human Resource'),
-    ('CoICT','CoICT'),
-    ('CoET','CoET'),
-    ('SoED','SoED'),
-    ('CoHU','CoHU'),
-    ('SoAF','SoAF'),
-    ('CoAF','CoAF'),
-    ('CoNAS','CoNAS'),
-    ('CoSS','CoSS'),
-    ('IDS','IDS'),
-    ('UDSoL','UDSoL'),
-    ('SJMC','SJMC'),
-    ('SoMG','SoMG'),
-    ('UDSE','UDSE'),
-    ('IKS','IKS'),
+    ('Human Resource', 'Human Resource'),
+    ('CoICT', 'CoICT'),
+    ('CoET', 'CoET'),
+    ('SoED', 'SoED'),
+    ('CoHU', 'CoHU'),
+    ('SoAF', 'SoAF'),
+    ('CoAF', 'CoAF'),
+    ('CoNAS', 'CoNAS'),
+    ('CoSS', 'CoSS'),
+    ('IDS', 'IDS'),
+    ('UDSoL', 'UDSoL'),
+    ('SJMC', 'SJMC'),
+    ('SoMG', 'SoMG'),
+    ('UDSE', 'UDSE'),
+    ('IKS', 'IKS'),
 )
 
 Role = (
-    ('RiskManager','RiskManager'),
-    ('RiskChampion','RiskChampion'),
+    ('RiskManager', 'RiskManager'),
+    ('RiskChampion', 'RiskChampion'),
 )
 
 StatusList = (
-    ('Active','Active'),
-    ('Pending','Pending'),
-    ('Rejected','Rejected'),
+    ('Active', 'Active'),
+    ('Pending', 'Pending'),
+    ('Rejected', 'Rejected'),
+)
+
+impacts = (
+    ('Very high', 'Very High'),
+    ('High', 'High'),
+    ('Moderate', 'Moderate'),
+    ('Low', 'Low'),
+    ('Very Low', 'Very Low'),
 )
 
 
 class Unit(models.Model):
     Unit_id = models.AutoField(primary_key=True)
-    Units = models.CharField(max_length=100,choices=UnitList)
-    
-# Risk table added
-class Risk(models.Model):
-    reporter=models.ForeignKey(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20,choices=Role)
-    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)  # Link to Department model
-    status = models.CharField(max_length=10,choices=StatusList)
-    last_updated = models.DateTimeField(auto_now=True)
-    
+    Units = models.CharField(max_length=100, choices=UnitList)
 
-    
+
+class Mitigation(models.Model):
+    id = models.AutoField(primary_key=True)
+    mitigation = models.CharField(max_length=400)
+    effectiveness = models.CharField(max_length=400)
+    weakness = models.CharField(max_length=400)
+
+
+class RiskDetails(models.Model):
+    id = models.AutoField(primary_key=True)
+    Causes = models.CharField(max_length=400)
+    consequences = models.CharField(max_length=500)
 
 
 class User(AbstractUser):
     id = models.AutoField(primary_key=True, default=None)
     fullname = models.CharField(max_length=200, default=None)
-    role = models.CharField(max_length=25,choices=Role, default=None)
-    unit = models.ForeignKey(Unit,on_delete=models.CASCADE, default=None)
-    
-    
+    role = models.CharField(max_length=25, choices=Role, default=None)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, default=None)
+
     @property
     def TotalRisk(self):
-        return self.risk_set.count() 
+        return self.risk_set.count()
 
     class Meta:
-    
         db_table = 'auth_user'
         verbose_name = 'user'
         verbose_name_plural = 'users'
@@ -70,3 +79,22 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.fullname
+
+
+# Risk table added
+class Risk(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    Description = models.CharField(max_length=400)
+    Details = models.ForeignKey(RiskDetails, on_delete=models.CASCADE)
+    reporter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    likelihood = models.CharField(max_length=50, choices=impacts)
+    impact = models.CharField(max_length=30, choices=impacts)
+    mitigation = models.ForeignKey(Mitigation, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=StatusList)
+    last_updated = models.DateTimeField(auto_now=True)
+
+
+
+
+    
