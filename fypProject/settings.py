bbc9aaa9df9481c0db5419e548733e4076539f8f
BonifasJK <bonifasjkomba@gmail.com>
2024-06-27 16:33:20 +0300

    jazzmin env updated and upload

--- fypProject/settings.py
+++ fypProject/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -160,177 +161,183 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+JAZZMIN_SETTINGS = {
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "RISK MANAGEMENT",
 
-#     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-#     "site_header": "RISK MANAGEMENT",
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "RISK MANAGEMENT",
 
-#     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-#     "site_brand": "RISK MANAGEMENT",
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    # "site_logo": "books/img/logo.png",
 
-#     # Logo to use for your site, must be present in static files, used for brand on top left
-#     # "site_logo": "books/img/logo.png",
+    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    "login_logo": "vendor/adminlte/img/AdminLTELogo1.png",
 
-#     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-#     "login_logo": None,
+    # Logo to use for login form in dark themes (defaults to login_logo)
+    "login_logo_dark": None,
 
-#     # Logo to use for login form in dark themes (defaults to login_logo)
-#     "login_logo_dark": None,
+    # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
 
-#     # CSS classes that are applied to the logo above
-#     "site_logo_classes": "img-circle",
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": None,
 
-#     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
-#     "site_icon": None,
+    # Welcome text on the login screen
+    "welcome_sign": "RISK MANAGEMENT",
 
-#     # Welcome text on the login screen
-#     "welcome_sign": "RISK MANAGEMENT",
+    # Copyright on the footer
+    "copyright": "RISK MANAGEMENT UNIT",
 
-#     # Copyright on the footer
-#     "copyright": "RISK MANAGEMENT UNIT",
+    # List of model admins to search from the search bar, search bar omitted if excluded
+    # If you want to use a single search field you dont need to use a list, you can use a simple string 
+    "search_model": ["auth.User", "auth.Group"],
 
-#     # List of model admins to search from the search bar, search bar omitted if excluded
-#     # If you want to use a single search field you dont need to use a list, you can use a simple string 
-#     "search_model": ["auth.User", "auth.Group"],
+    # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
+    "user_avatar": None,
 
-#     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
-#     "user_avatar": None,
+    ############
+    # Top Menu #
+    ############
 
-#     ############
-#     # Top Menu #
-#     ############
+    # Links to put along the top menu
+    "topmenu_links": [
 
-#     # Links to put along the top menu
-#     "topmenu_links": [
+        # Url that gets reversed (Permissions can be added)
+        {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
 
-#         # Url that gets reversed (Permissions can be added)
-#         {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
+        # external url that opens in a new window (Permissions can be added)
+        # {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
 
-#         # external url that opens in a new window (Permissions can be added)
-#         {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
+        # model admin to link to (Permissions checked against model)
+        {"model": "auth.User"},
 
-#         # model admin to link to (Permissions checked against model)
-#         {"model": "auth.User"},
+        # App with dropdown menu to all its models pages (Permissions checked against models)
+        {"app": "books"},
+    ],
 
-#         # App with dropdown menu to all its models pages (Permissions checked against models)
-#         {"app": "books"},
-#     ],
+    #############
+    # User Menu #
+    #############
 
-#     #############
-#     # User Menu #
-#     #############
+    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
+    "usermenu_links": [
+        # {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
+        {"model": "auth.user"}
+    ],
 
-#     # Additional links to include in the user menu on the top right ("app" url type is not allowed)
-#     "usermenu_links": [
-#         {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
-#         {"model": "auth.user"}
-#     ],
+    #############
+    # Side Menu #
+    #############
 
-#     #############
-#     # Side Menu #
-#     #############
+    # Whether to display the side menu
+    "show_sidebar": True,
 
-#     # Whether to display the side menu
-#     "show_sidebar": True,
+    # Whether to aut expand the menu
+    "navigation_expanded": True,
 
-#     # Whether to aut expand the menu
-#     "navigation_expanded": True,
+    # Hide these apps when generating side menu e.g (auth)
+    "hide_apps": ['Support'],
 
-#     # Hide these apps when generating side menu e.g (auth)
-#     "hide_apps": [],
+    # Hide these models when generating side menu (e.g auth.user)
+    "hide_models": ["url.Support"],
 
-#     # Hide these models when generating side menu (e.g auth.user)
-#     "hide_models": [],
+    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
+    "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
 
-#     # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
-#     "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
+    # Custom links to append to app groups, keyed on app name
+    "custom_links": {
+        "books": [{
+            "name": "Make Messages", 
+            "url": "make_messages", 
+            "icon": "fas fa-comments",
+            "permissions": ["books.view_book"]
+        }]
+    },
 
-#     # Custom links to append to app groups, keyed on app name
-#     "custom_links": {
-#         "books": [{
-#             "name": "Make Messages", 
-#             "url": "make_messages", 
-#             "icon": "fas fa-comments",
-#             "permissions": ["books.view_book"]
-#         }]
-#     },
+    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
+    # for the full list of 5.13.0 free icon classes
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "udsm_risk_register.Mitigations": "fas fa-user", 
+        # "udsm_risk_register.RiskDetails": "fa-solid fa-circle-info",
+        "udsm_risk_register.Risks": "",
+        "udsm_risk_register.Units": "fas fa-building",
+        "udsm_risk_register.Users": "fas fa-user",
+        
+        
+        
+    },
+    # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
 
-#     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
-#     # for the full list of 5.13.0 free icon classes
-#     "icons": {
-#         "auth": "fas fa-users-cog",
-#         "auth.user": "fas fa-user",
-#         "auth.Group": "fas fa-users",
-#         "udsm_risk_register.Users": "fas fa-users",
-#         "udsm_risk_register.Units": "fas fa-building",
-#     },
-#     # Icons that are used when one is not manually specified
-#     "default_icon_parents": "fas fa-chevron-circle-right",
-#     "default_icon_children": "fas fa-circle",
+    #################
+    # Related Modal #
+    #################
+    # Use modals instead of popups
+    "related_modal_active": False,
 
-#     #################
-#     # Related Modal #
-#     #################
-#     # Use modals instead of popups
-#     "related_modal_active": True,
+    #############
+    # UI Tweaks #
+    #############
+    # Relative paths to custom CSS/JS scripts (must be present in static files)
+    "custom_css": None,
+    "custom_js": None,
+    # Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)
+    "use_google_fonts_cdn": True,
+    # Whether to show the UI customizer on the sidebar
+    "show_ui_builder": False,
 
-#     #############
-#     # UI Tweaks #
-#     #############
-#     # Relative paths to custom CSS/JS scripts (must be present in static files)
-#     "custom_css": None,
-#     "custom_js": None,
-#     # Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)
-#     "use_google_fonts_cdn": True,
-#     # Whether to show the UI customizer on the sidebar
-#     "show_ui_builder": True,
+    # ###############
+    # # Change view #
+    # ###############
+    # # Render out the change view as a single form, or in tabs, current options are
+    # - single
+    # - horizontal_tabs (default)
+    # - vertical_tabs
+    # - collapsible
+    # - carousel
+    "changeform_format": "horizontal_tabs",
+    # override change forms on a per modeladmin basis
+    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+    # Add a language dropdown into the admin
+    "language_chooser": False,
+}
 
-#     ###############
-#     # Change view #
-#     ###############
-#     # Render out the change view as a single form, or in tabs, current options are
-#     # - single
-#     # - horizontal_tabs (default)
-#     # - vertical_tabs
-#     # - collapsible
-#     # - carousel
-#     "changeform_format": "horizontal_tabs",
-#     # override change forms on a per modeladmin basis
-#     "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
-#     # Add a language dropdown into the admin
-#     "language_chooser": False,
-# }
-
-# JAZZMIN_UI_TWEAKS = {
-#     "navbar_small_text": False,
-#     "footer_small_text": False,
-#     "body_small_text": False,
-#     "brand_small_text": False,
-#     "brand_colour": "navbar-info",
-#     "accent": "accent-lightblue",
-#     "navbar": "navbar-info navbar-dark",
-#     "no_navbar_border": False,
-#     "navbar_fixed": False,
-#     "layout_boxed": False,
-#     "footer_fixed": False,
-#     "sidebar_fixed": False,
-#     "sidebar": "sidebar-dark-primary",
-#     "sidebar_nav_small_text": False,
-#     "sidebar_disable_expand": False,
-#     "sidebar_nav_child_indent": False,
-#     "sidebar_nav_compact_style": False,
-#     "sidebar_nav_legacy_style": False,
-#     "sidebar_nav_flat_style": False,
-#     "theme": "flatly",
-#     "dark_mode_theme": None,
-#     "button_classes": {
-#         "primary": "btn-outline-primary",
-#         "secondary": "btn-outline-secondary",
-#         "info": "btn-info",
-#         "warning": "btn-warning",
-#         "danger": "btn-danger",
-#         "success": "btn-success"
-#     }
-# }
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-info",
+    "accent": "accent-lightblue",
+    "navbar": "navbar-info navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "flatly",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-outline-primary",
+        "secondary": "btn-outline-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
 
 DYNAMIC_DATATB = {
     # SLUG -> Import_PATH 
